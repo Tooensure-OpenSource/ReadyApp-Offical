67bbe7dd48b1cbbe45878e87bb81851cdb588f91
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-05 12:08:54 -0500

    Adding user auth repo implementing interface

---
+++ Ecommerce.Data/Repositories/UserRepositories/UserAuthRepository.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Data.Repositories.IRepositories.IUserRepositories;
+using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Data.Repositories.UserRepositories
+{
+    public class UserAuthRepository : IUserAuthRepository
+    {
+        public ServiceResponse<Guid> Login(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExistByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExistById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExistByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
