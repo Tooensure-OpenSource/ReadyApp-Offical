0bd4ba7e1cf4b7db561d307bde6abff6d9f8ba1e
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-05 20:44:56 -0500

    Adding grpc client proto buf

--- ReadyApp.Ui.Console/Program.cs
+++ ReadyApp.Ui.Console/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+
+
+//var channel = GrpcChannel.ForAddress("https://localhost:5001");
+/*
+var client = new UserAuth;
+
+var response = await client.SayHelloAsync(
+    new HelloRequest { Name = "World" });
+
+Console.WriteLine(response.Message);
+*/
