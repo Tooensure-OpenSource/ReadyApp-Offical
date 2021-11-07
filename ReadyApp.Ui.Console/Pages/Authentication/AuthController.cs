9919d5584a0e27d9c09187d5f462b5f0d66146ea
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-06 19:30:18 -0500

    Client commit, Login registration calls

---
+++ ReadyApp.Ui.Console/Pages/Authentication/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Client.Console.Pages.Authentication
+{
+    public static class AuthController
+    {
+        public static void Init(string httpRoute)
+        {
+            do
+            {
+
+                System.Console.WriteLine("Login : 1, Register : 2");
+                var option = System.Console.ReadLine();
+                if (option == "1")
+                {
+                    AuthLogin(httpRoute);
+                }
+                else if (option == "2") AuthRegister(httpRoute);
+
+            } while (!Login.LoginSuccessful);
+            if (Login.LoginSuccessful) System.Console.WriteLine("Login Successful");
+        }
+
+        public static void AuthLogin(string httpRoute)
+        {
+            System.Console.WriteLine("Login");
+            Login.Init(httpRoute);
+        }
+        public static void AuthRegister(string httpRoute)
+        {
+            System.Console.WriteLine("Register");
+            Register.Init(httpRoute);
+        }
+    }
+}
--- ReadyApp.Ui.Console/Program.cs
+++ ReadyApp.Ui.Console/Program.cs
@@ -1,20 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ReadyApp.Client.Console.Pages.Authentication;
 
-using Ecommerce.Server.Grpc.Client;
-using Grpc.Net.Client;
-
-
-var user = new UserRegisterDto
-{
-    Username = "Frank",
-    Email = "frank@gmail.com",
-    Password = "1234"
-};
-
-var channel = GrpcChannel.ForAddress("http://localhost:5186");
-
-var client = new UserAuth.UserAuthClient(channel);
-
-var response = await client.UserRegisterAsync(user);
-
-Console.WriteLine($"{response.Data}, {response.IsSuccessful}, {response.Message}");
+Console.WriteLine("The Ready App | From Tooensure");
+AuthController.Init("http://localhost:5186");
