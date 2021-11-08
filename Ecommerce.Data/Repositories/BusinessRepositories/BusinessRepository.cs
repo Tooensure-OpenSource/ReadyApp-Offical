bf4f6ae94a55c7096e4b392f243ad807137bc584
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-07 19:42:31 -0600

    Business repo implementation

---
+++ Ecommerce.Data/Repositories/BusinessRepositories/BusinessRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Data.Repositories.IRepositories.IBusinessRepositories;
+using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Data.Repositories.BusinessRepositories
+{
+    public class BusinessRepository : IBusinessRepository
+    {
+        public bool ExistByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<Guid> Register(Owner owner, Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
