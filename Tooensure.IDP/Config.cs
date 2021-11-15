50efe79cdb302a3f19bf9d22979919578cf38abe
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-15 04:50:06 -0600

    Default project reset

---
+++ Tooensure.IDP/Config.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Tooensure.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client
+                {
+                    ClientId = "userclient",
+                    ClientName = "User login",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>()
+                    {
+                        "http://localhost:5186"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("sercret".Sha256())
+                    }
+                }
+            };
+    }
+}
---
+++ Tooensure.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "1",
+                        Username = "@shawnDoe",
+                        Password = "12345",
+
+                        Claims = new List<Claim>
+                        {
+                            new Claim("given_name", "Shawn"),
+                            new Claim("family_name", "Doe"),
+                        }
+                    },
+
+                    new TestUser
+                    {
+                        SubjectId = "2",
+                        Username = "@frank",
+                        Password = "12345",
+
+                        Claims = new List<Claim>
+                        {
+                            new Claim("given_name", "Frank"),
+                            new Claim("family_name", "Joe"),
+                        }
+                    },
+                };
+            }
+        }
+    }
+}
