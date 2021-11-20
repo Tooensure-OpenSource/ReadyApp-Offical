537b6be16ecb6aae46e3e3e86c95f14cd2c4a18b
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-20 00:13:40 -0600

    Adding tests to project

---
+++ Ecommerce.Test/Features/BusinessFeatureShould.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tooensure.DataStructure.RepositoryPattern;
+using Xunit;
+
+namespace Ecommerce.Test.Features
+{
+    public class BusinessFeatureShould
+    {
+        [Fact]
+        public void RegisterWithUserAsOwner()
+        {
+            /* Arrange */
+            // Object models
+            User user = new("tooensure@gmail.com", "passwordForHash") { FirstName = "Shawn", LastName = "Doe", Username = "shaawndoe", Token = "1234" };
+            Business business = new(user) { Name = "tooensure llc", Username = "tooensure" };
+            // Mocking DbSet allows implementation of DbSet at runtime
+            var mockDbContext = new Mock<DataContext>();
+            var mockDbSet = new Mock<DbSet<Business>>();
+            var uow = new UnitOfWork(mockDbContext.Object);
+
+            mockDbContext.Setup(x => x.Businesses).Returns(mockDbSet.Object);
+            mockDbContext.Setup(p => p.Set<Business>()).Returns(mockDbSet.Object);
+            mockDbContext.Setup(p => p.Add(It.IsAny<Business>())).Returns(mockDbSet.Object.Add);
+
+            uow.Businesses.Add(business);
+
+            mockDbSet.Verify(m => m.Add(It.IsAny<Business>()), Times.Once());
+            mockDbContext.Verify(m => m.SaveChanges(), Times.Exactly(3));
+        }
+    }
+}
---
+++ Ecommerce.Test/SearchFeatureShould.cs
@@ -0,0 +1,47 @@
+using AutoMapper;
+using Ecommerce.Data;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Models.Features;
+using Ecommerce.Server.Grpc;
+using Ecommerce.Server.Grpc.Services;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Tooensure.DataStructure.RepositoryPattern;
+using Tooensure.DataStructure.RepositoryPattern.Repositorties;
+using Xunit;
+
+namespace Ecommerce.Test
+{
+    public class SearchFeatureShould
+    {
+        public User User { get; set; }
+        [Fact]
+        public void ValidateSearchInput()
+        {
+            // System Under Test
+            // Arrange
+            SearchFeature sut1 = new("ImNotEmpty");
+            SearchFeature sut2 = new("");
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            SearchFeature sut3 = new(null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+
+
+            // Act
+            bool ValidValidation1 = sut1.IsValid;
+            bool FalseValidation1 = sut2.IsValid;
+            bool FalseValidation2 = sut3.IsValid;
+
+
+            // Assert
+            Assert.True(ValidValidation1);
+            Assert.False(FalseValidation1);
+            Assert.False(FalseValidation2);
+
+        }
+    }
+}
