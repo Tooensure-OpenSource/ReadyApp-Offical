0dd18b12cf6c8a019c729dfc94fd229c0e92e99c
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-26 12:46:14 -0600

    Mobile project

---
+++ ReadyApp.UI.Mobile/ReadyApp.UI.Mobile/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReadyApp.UI.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnBtnLoginClicked(object sender, EventArgs e)
+        {
+           
+            //LoginLable.Text = "Logging in.......";
+        }
+    }
+}
---
+++ ReadyApp.UI.Mobile/ReadyApp.UI.Mobile/RegisterUserPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReadyApp.UI.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterUserPage : ContentPage
+    {
+        public RegisterUserPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
