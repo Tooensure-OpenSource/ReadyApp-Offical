6f0c532212fbcefee4cb17a05c46fd6b0ceb8a8f
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-29 05:25:23 -0600

    Creation Dto made by the client to REST server

---
+++ Ecommerce.Domain/Models/UserCreationDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Domain.Models
+{
+    public class UserCreationDto
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required, EmailAddress]
+        public string? EmailAddress { get; set; }
+
+    }
+}
