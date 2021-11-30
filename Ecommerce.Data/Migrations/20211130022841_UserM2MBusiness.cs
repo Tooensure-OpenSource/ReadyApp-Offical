7173edf62839f33d23ae927b242bb31b3db6d1e8
Shawn Bellazan <tooensuremaster@gmail.com>
2021-11-29 22:18:38 -0600

    Migrations User Business Many2Many relationship

---
+++ Ecommerce.Data/Migrations/20211130022841_UserM2MBusiness.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Data.Migrations
+{
+    public partial class UserM2MBusiness : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "BusinessUser",
+                columns: table => new
+                {
+                    ConsumersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FavoriteBusinessesBusinessId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessUser", x => new { x.ConsumersId, x.FavoriteBusinessesBusinessId });
+                    table.ForeignKey(
+                        name: "FK_BusinessUser_Businesses_FavoriteBusinessesBusinessId",
+                        column: x => x.FavoriteBusinessesBusinessId,
+                        principalTable: "Businesses",
+                        principalColumn: "BusinessId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BusinessUser_Users_ConsumersId",
+                        column: x => x.ConsumersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessUser_FavoriteBusinessesBusinessId",
+                table: "BusinessUser",
+                column: "FavoriteBusinessesBusinessId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusinessUser");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
