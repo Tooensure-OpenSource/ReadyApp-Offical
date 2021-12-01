1acf5e44b8074f210607d82a0a4e5f811ba5092f
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-01 02:44:41 -0600

    Creation and  Login Dto, Will be renamed

--- Ecommerce.Domain/Models/UserCreationDto.cs
+++ Ecommerce.Domain/Models/UserCreationDto.cs
@@ -5,18 +5,18 @@ namespace Ecommerce.Domain.Models
     public class UserCreationDto
     {
         [Required]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
         [Required]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
 
         [Required, EmailAddress]
-        public string? EmailAddress { get; set; }
+        public string EmailAddress { get; set; }
 
     }
 }
