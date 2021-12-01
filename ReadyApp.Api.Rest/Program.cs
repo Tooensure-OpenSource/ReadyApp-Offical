3386f1bcba0db4dae12cdc51b4cf7a414cbaf2bc
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-01 02:50:25 -0600

    Some services configurations

--- ReadyApp.Api.Rest/Program.cs
+++ ReadyApp.Api.Rest/Program.cs
@@ -1,19 +1,35 @@
 using Ecommerce.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Tooensure.DataStructure.RepositoryPattern;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 1. Add Authentication Services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = "https://tooensure-dev.us.auth0.com/";
+    options.Audience = "https://tooensure/readyapp/api";
+});
+
 builder.Services.AddDbContext<DataContext>(opt =>
         opt.UseSqlServer(builder.Configuration.GetConnectionString("ReadyAppDb"))
         .EnableSensitiveDataLogging()
         .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -25,7 +41,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
