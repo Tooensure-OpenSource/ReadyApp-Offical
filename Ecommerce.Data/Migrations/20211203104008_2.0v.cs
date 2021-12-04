de8f2c5915c88c93746cc91b1ca5f473e5045767
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-03 21:47:33 -0600

    Some migration files

---
+++ Ecommerce.Data/Migrations/20211203104008_2.0v.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Data.Migrations
+{
+    public partial class _20v : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Businesses_BusinessId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "BusinessUser");
+
+            migrationBuilder.DropTable(
+                name: "FriendListUser");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FriendListId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BusinessId",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Businesses",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FriendListId",
+                table: "Users",
+                column: "FriendListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Businesses_UserId",
+                table: "Businesses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Businesses_Users_UserId",
+                table: "Businesses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Businesses_BusinessId",
+                table: "Employees",
+                column: "BusinessId",
+                principalTable: "Businesses",
+                principalColumn: "BusinessId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_FriendLists_FriendListId",
+                table: "Users",
+                column: "FriendListId",
+                principalTable: "FriendLists",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Businesses_Users_UserId",
+                table: "Businesses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Businesses_BusinessId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_FriendLists_FriendListId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_FriendListId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Businesses_UserId",
+                table: "Businesses");
+
+            migrationBuilder.DropColumn(
+                name: "FriendListId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Businesses");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BusinessId",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateTable(
+                name: "BusinessUser",
+                columns: table => new
+                {
+                    ConsumersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FavoriteBusinessesBusinessId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessUser", x => new { x.ConsumersId, x.FavoriteBusinessesBusinessId });
+                    table.ForeignKey(
+                        name: "FK_BusinessUser_Businesses_FavoriteBusinessesBusinessId",
+                        column: x => x.FavoriteBusinessesBusinessId,
+                        principalTable: "Businesses",
+                        principalColumn: "BusinessId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BusinessUser_Users_ConsumersId",
+                        column: x => x.ConsumersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FriendListUser",
+                columns: table => new
+                {
+                    FriendsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendListUser", x => new { x.FriendsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_FriendListUser_FriendLists_FriendsId",
+                        column: x => x.FriendsId,
+                        principalTable: "FriendLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FriendListUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessUser_FavoriteBusinessesBusinessId",
+                table: "BusinessUser",
+                column: "FavoriteBusinessesBusinessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendListUser_UsersId",
+                table: "FriendListUser",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Businesses_BusinessId",
+                table: "Employees",
+                column: "BusinessId",
+                principalTable: "Businesses",
+                principalColumn: "BusinessId");
+        }
+    }
+}
