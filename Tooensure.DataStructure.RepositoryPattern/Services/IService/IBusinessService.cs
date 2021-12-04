f78a71d3ee85a866683ae720912e706286a704ea
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-03 22:00:42 -0600

    repo service inheritance business service

---
+++ Tooensure.DataStructure.RepositoryPattern/Services/IService/IBusinessService.cs
@@ -0,0 +1,8 @@
+using Ecommerce.Domain.Entities;
+
+namespace Tooensure.DataStructure.RepositoryPattern.Services.IService
+{
+    public interface IBusinessService : IRepositoryService<Business>
+    {
+    }
+}
