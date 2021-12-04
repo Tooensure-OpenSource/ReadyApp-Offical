4cbb6cf67a188ed3839c0370f94a17a2d401522f
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-03 21:52:28 -0600

    Get add, recieve user Dtos

---
+++ Ecommerce.Domain/Models/User/AddUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Domain.Models.User
+{
+    public class AddUserDto
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required, EmailAddress]
+        public string? EmailAddress { get; set; }
+
+    }
+}
---
+++ Ecommerce.Domain/Models/User/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Domain.Models.User
+{
+    public class UserDto
+    {
+        public Guid UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Username { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
